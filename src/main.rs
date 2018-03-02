4baa77c6aad0cbbddab5c76b80256a20c52d918f
Noah <nbtheduke@gmail.com>
2018-03-02 15:38:17 -0500

    Added arr_ack

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,204 @@
+#![allow(dead_code)]
+#![allow(unused_imports)]
 extern crate time;
 
+use std::collections::HashMap;
+use std::cmp::max;
+use std::mem;
 use time::PreciseTime;
 
 fn main() {
     let m = 3;
 
-    for n in 1..20 {
+    for n in [20, 25, 30].iter() {
         println!("n: {}", n);
-        // let s1 = PreciseTime::now();
-        // let res1 = ack(m, n);
-        // let e1 = PreciseTime::now();
-        // println!("ack -> {} took {}", res1, s1.to(e1));
+
+        let s0 = PreciseTime::now();
+        let res0 = fast_ackermann(m, *n);
+        let e0 = PreciseTime::now();
+        println!("ack_with_cache -> {} took {}", res0, s0.to(e0));
+
+        let s1 = PreciseTime::now();
+        let res1 = fast_ackermann(m, *n);
+        let e1 = PreciseTime::now();
+        println!("fast_ackermann -> {} took {}", res1, s1.to(e1));
 
         let s2 = PreciseTime::now();
-        let res2 = a_opt(m, n);
+        let res2 = vec_ack(m, *n);
         let e2 = PreciseTime::now();
-        println!("opt -> {} took {}", res2, s2.to(e2));
+        println!("vec_ack -> {} took {}", res2, s2.to(e2));
+
+        let s3 = PreciseTime::now();
+        let res3 = arr_ack(m, *n);
+        let e3 = PreciseTime::now();
+        println!("arr_ack -> {} took {}", res3, s3.to(e3));
     }
+}
+
+fn ack_with_cache(m: u64, n: u64) -> u64 {
+    let mut cache: HashMap<(u64, u64), u64> = HashMap::new();
+
+    _ack_with_cache(&mut cache, m, n)
+}
+
+fn _ack_with_cache(cache: &mut HashMap<(u64, u64), u64>, m: u64, n: u64) -> u64 {
+    match (m, n) {
+        (0, n) => n + 1,
+        (1, n) => n + 2,
+        (m, 0) => _ack_with_cache(cache, m - 1, 1),
+        (m, 1) => {
+            let n = _ack_with_cache(cache, m - 1, 1);
+            _ack_with_cache(cache, m - 1, n)
+        }
+        (m, n) => {
+            if cache.contains_key(&(m, n)) {
+                *cache.get(&(m, n)).unwrap()
+            } else {
+                let s = _ack_with_cache(cache, m, n - 2);
+                let t = _ack_with_cache(cache, m, n - 1);
+
+                let res = (s..(t + 1)).fold(0, |acc, x| _ack_comparator(cache, m, acc, x));
+                cache.insert((m, n), res);
+                res
+            }
+        }
+    }
+}
+
+fn _ack_comparator(cache: &mut HashMap<(u64, u64), u64>, m: u64, acc: u64, x: u64) -> u64 {
+    let c = _ack_with_cache(cache, m - 1, x);
+    max(acc, c)
 }
 
 #[derive(Clone)]
 struct Record {
-    f: u64,
-    s: u64,
-    t: Option<Box<Record>>,
+    result: u64,
+    previous_result: u64,
+    cache: Option<Box<Record>>,
 }
 
 impl Record {
-    fn new(f: u64, s: u64, t: Option<Box<Record>>) -> Record {
-        Record { f: f, s: s, t: t }
+    fn new(result: u64, previous_result: u64, cache: Option<Box<Record>>) -> Record {
+        Record { result, previous_result, cache }
     }
 }
 
-fn a_opt(i: u64, n: u64) -> u64 {
-    let mut v = Record::new(1, 0, None);
-    for k in 1..(i + 1) {
-        v = a_use_p(k - 1, 1, v)
+fn fast_ackermann(m: u64, n: u64) -> u64 {
+    let mut cache = Record::new(1, 0, None);
+    for m_builder in 0..m {
+        cache = ack_with_incrementalization(m_builder, 1, cache)
+    }
+    for n_builder in 1..(n + 1) {
+        cache = ack_with_incrementalization(m, n_builder, cache)
+    }
+    cache.result
+}
+
+fn ack_with_incrementalization(m: u64, n: u64, current_cache: Record) -> Record {
+    if m == 0 {
+        Record::new(n + 1, 0, None)
+    } else if m == 1 {
+        Record::new(n + 2, 0, None)
+    } else if n == 0 {
+        let mut new_cache = Record::new(1, 0, None);
+        for m_builder in 0..m {
+            new_cache = ack_with_incrementalization(m_builder, 1, new_cache);
+        }
+        new_cache
+    } else if n == 1 {
+        let cache_result = current_cache.result;
+        let mut new_cache = current_cache;
+        for n_builder in 2..(cache_result + 1) {
+            new_cache = ack_with_incrementalization(m - 1, n_builder, new_cache);
+        }
+        Record::new(new_cache.result, cache_result, Some(Box::new(new_cache)))
+    } else {
+        let cache_result = current_cache.result;
+        let mut new_cache = *current_cache.cache.unwrap();
+        for n_builder in (current_cache.previous_result + 1)..(current_cache.result + 1) {
+            new_cache = ack_with_incrementalization(m - 1, n_builder, new_cache);
+        }
+        Record::new(new_cache.result, cache_result, Some(Box::new(new_cache)))
+    }
+}
+
+fn vec_ack(m: u64, n: u64) -> u64 {
+    let mut v: Vec<u64> = Vec::with_capacity(6);
+    v.push(1);
+    for k in 0..m {
+        v = a_use_p2(k, 1, v)
     }
     for j in 1..(n + 1) {
-        v = a_use_p(i, j, v)
+        v = a_use_p2(m, j, v)
     }
-    v.f
+    v[0]
 }
 
-fn a_use_p(i: u64, n: u64, r_use: Record) -> Record {
-    if i == 0 {
-        Record::new(n + 1, 0, None)
+fn a_use_p2(m: u64, n: u64, r_use: Vec<u64>) -> Vec<u64> {
+    if m == 0 {
+        vec![n + 1]
+    } else if m == 1 {
+        vec![n + 2]
     } else if n == 0 {
-        let mut v = Record::new(1, 0, None);
-        for k in 1..(i + 1) {
-            v = a_use_p(k - 1, 1, v);
-        }
-        v
-    } else if n - 1 == 0 {
-        let v11 = r_use.f;
+        (0..m).fold(vec![1], |v, k| a_use_p2(k, 1, v))
+    } else if n == 1 {
+        let v11 = r_use[0];
         let mut v2 = r_use.clone();
-        for k in 2..(r_use.f + 1) {
-            v2 = a_use_p(i - 1, k, v2);
+        for k in 2..(v11 + 1) {
+            v2 = a_use_p2(m - 1, k, v2);
         }
-        Record::new(v2.f, v11, Some(Box::new(v2)))
+        let v3 = v2[0];
+        v2.insert(0, v3);
+        v2.insert(1, v11);
+        v2
     } else {
-        let v11 = r_use.f;
-        let mut v2 = *r_use.t.unwrap();
-        for k in (r_use.s + 1)..(r_use.f + 1) {
-            v2 = a_use_p(i - 1, k, v2);
+        let v11 = r_use[0];
+        let mut v2 = r_use[2..].to_owned();
+        for k in (r_use[1] + 1)..(v11 + 1) {
+            v2 = a_use_p2(m - 1, k, v2);
         }
-        Record::new(v2.f, v11, Some(Box::new(v2)))
+        let v3 = v2[0];
+        v2.insert(0, v3);
+        v2.insert(1, v11);
+        v2
+    }
+}
+
+fn arr_ack(m: u64, n: u64) -> u64 {
+    let mut cache: [u64; 6] = [0; 6];
+    cache[0] = 1;
+    for m_builder in 0..m {
+        cache = _arr_ack(m_builder, 1, cache)
+    }
+    for n_builder in 1..(n + 1) {
+        cache = _arr_ack(m, n_builder, cache)
+    }
+    cache[0]
+}
+
+fn _arr_ack(m: u64, n: u64, mut cache: [u64; 6]) -> [u64; 6] {
+    if m == 0 {
+        [n + 1, 0, 0, 0, 0, 0]
+    } else if m == 1 {
+        [n + 2, 0, 0, 0, 0, 0]
+    } else if n == 0 {
+        let mut new_cache = [1, 0, 0, 0, 0, 0];
+        for m_builder in 0..m {
+            new_cache = _arr_ack(m_builder, 1, new_cache);
+        }
+        new_cache
+    } else if n == 1 {
+        let previous_result = cache[0];
+        for n_builder in 2..(previous_result + 1) {
+            cache = _arr_ack(m - 1, n_builder, cache);
+        }
+        [cache[0], previous_result, cache[0], cache[1], cache[2], cache[3]]
+    } else {
+        let previous_result = cache[0];
+        let mut new_cache = [cache[2], cache[3], cache[4], cache[5], 0, 0];
+        for n_builder in (cache[1] + 1)..(previous_result + 1) {
+            new_cache = _arr_ack(m - 1, n_builder, new_cache);
+        }
+        [new_cache[0], previous_result, new_cache[0], new_cache[1], new_cache[2], new_cache[3]]
     }
 }
